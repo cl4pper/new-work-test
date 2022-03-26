2631e21395f2c16f0052203015daafe01c1e5de1
cl4pper <cl4pper@gmail.com>
2022-03-26 21:44:09 +0000

    chore: setup aliases and update gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
 	},
 	resolve: {
 		extensions: ['.js', '.jsx'],
+		alias: {
+			'@components': path.resolve(__dirname, 'src/components'),
+		}
 	},
 	devServer: {
 		static: path.resolve(__dirname, 'build'),
