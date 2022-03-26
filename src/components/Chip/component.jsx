6da7128e77d31357ef7bf00cb26608ef76bde04a
cl4pper <cl4pper@gmail.com>
2022-03-26 22:08:51 +0000

    style: lint Chip component
    * chore: remove auto open from run:dev
    * PAUSE TO EAT

--- src/components/Chip/component.jsx
+++ src/components/Chip/component.jsx
@@ -5,14 +5,14 @@ import { Text } from '@components';
 // STYLE
 import './style.scss';
 
-const Chip = props => {
-    const { id, label, active } = props;
-    
-    return (
-        <div id={id} data-testid={id} className={classnames('Chip', { 'Chip--active': active })}>
-            <Text id={`${id}-chip-label`} content={label} small />
-        </div>
-    );
+const Chip = (props) => {
+	const { id, label, active } = props;
+
+	return (
+		<div id={id} data-testid={id} className={classnames('Chip', { 'Chip--active': active })}>
+			<Text id={`${id}-chip-label`} content={label} small />
+		</div>
+	);
 };
 
 export default Chip;
